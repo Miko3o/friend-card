7d9cbbeafd17f851aa10a3dc70b57b61568c2339
Miko3o <mikomunoz459@gmail.com>
2024-04-21 20:13:38 -0700

    feature(abstraction): abstracted the background and bottom bar colors (note: tailwind requires full color string ex: bg-orange-300)

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -7,11 +7,13 @@ import placeholder_person from './assets/images/chao.png'
 import './App.css'
 
 function App() {
+const [backgroundColor, setBackgroundColor] = useState<string>('bg-gradient-to-t from-orange-300');
+const [bottomBarColor, setBottomBarColor] = useState<string>('bg-orange-100');
 
   return (
     <>
       <div className="flex justify-center h-screen w-screen bg-slate-300">
-        <MainCard name='chao' fav_song='walking on sunshine' profile_pic={placeholder_person}/>
+        <MainCard name='chao' fav_song='walking on sunshine' profile_pic={placeholder_person} background_color={backgroundColor} bottom_bar_color={bottomBarColor}/>
       </div>
     </>
   )
--- frontend/client/src/components/MainCard.tsx
+++ frontend/client/src/components/MainCard.tsx
@@ -6,6 +6,8 @@ type MainCardProps = {
     name: string
     fav_song: string
     profile_pic: string
+    background_color: string
+    bottom_bar_color: string
 
 
 }
@@ -15,11 +17,11 @@ export const MainCard = (props: MainCardProps) => {
     
     return (
         <div className="m-28 w-[800px] h-[450px] bg-white flex justify-center items-center shadow-2xl"> {/*wrapper*/}
-            <div className="w-[790px] h-[440px] bg-gradient-to-t from-orange-300 to bg-transparent flex justify-end items-end"> {/*orange background*/}
+            <div className={`w-[790px] h-[440px] ${props.background_color} to bg-transparent flex justify-end items-end`}> {/*background*/}
                 <div className='flex justify-end mr-16'>
                     <img src={props.profile_pic} alt="person" width="270" style={{ transform: 'scaleX(-1)' }}/>
                 </div>
-                <div className="flex absolute w-[790px] h-[100px] rounded-t-[50%] rounded-tl-[40%] rounded-tr-[40%] bg-gradient-to-t from-white to bg-orange-100 opacity-90"   > {/*bottom white bar*/}
+                <div className={`flex absolute w-[790px] h-[100px] rounded-t-[50%] rounded-tl-[40%] rounded-tr-[40%] bg-gradient-to-t from-white to ${props.bottom_bar_color} opacity-90"`}> {/*bottom white bar*/}
                     <h1 className='text-5xl my-7 mx-32'>
                        {props.name} 
                     </h1>
