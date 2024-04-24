4cac5541474d7557d9ff3146e47092742588a232
Miko3o <mikomunoz459@gmail.com>
2024-04-24 05:42:59 -0700

    fix(.env): got the env file working and changed the clientId and secret

--- frontend/client/vite.config.ts
+++ frontend/client/vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+
+dotenv.config({ path: resolve(__dirname, '.env')})
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
