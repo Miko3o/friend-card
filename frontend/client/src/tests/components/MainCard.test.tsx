fe42b57364ca999688f76faed6f953e8b7d2cc61
Miko3o <mikomunoz459@gmail.com>
2024-04-27 01:27:44 -0700

    test(MainCard): tried to set up a test but having trouble with an expect error, might come back to it later

---
+++ frontend/client/src/tests/components/MainCard.test.tsx
@@ -0,0 +1,35 @@
+import { it, expect, describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+
+import { MainCard } from '../../components/MainCard'
+import { SpotifyLogin } from '../../components/SpotifyLogin'
+import { Dashboard } from '../../components/Dashboard'
+
+const mockSpotifyLogin = jest.fn()
+jest.mock('@/components/MainCard', () => ({
+    default: (props: any) => {
+        mockSpotifyLogin(props)
+        return <div>SpotifyLogin</div>
+    }
+}))
+
+const mockDashboard = jest.fn()
+jest.mock('@/components/Dashboard', () => ({
+    default: (props: any) => {
+        mockDashboard(props)
+        return <div>Dashboard</div>
+    }
+}))
+
+describe('MainCard', () => {
+    it('should render the MainCard with the name when name is provided', () => {
+        render(<MainCard code={'12345'} name='chao' fav_song='walking on sunshine' profile_pic={'img.png'} background_color={'blue'} bottom_bar_color={'red'}/>)
+
+        const heading = screen.getByRole('heading')
+        expect(heading).toBeInTheDocument
+
+        screen.debug();
+    })
+})
